dbdf1d3f58812cff65b79a4ff2c36896e75cb3c2
doddu <doddu@qq.com>
2018-03-26 18:33:46 +0800

    多语言， login页面增加英文配置

--- src/common/lang/en.js
+++ src/common/lang/en.js
@@ -1,14 +1,8 @@
+import login from './en/login'
 const ln = {
   common: {
-    Login: 'Login'
+    Login: 'Log in'
   },
-  login: {
-    tips: {
-      name: 'Please enter your user name',
-      pwd: 'Please enter your password'
-    },
-    welcome: 'Welcome',
-    forgotPwd: 'Forgot password'
-  }
+  login: login
 }
 export default ln
---
+++ src/common/lang/en/login.js
@@ -0,0 +1,10 @@
+const ln = {
+  tips: {
+    name: 'Please enter your user name',
+    pwd: 'Please enter your password'
+  },
+  welcome: 'Welcome',
+  forgotPwd: 'Forgot password'
+}
+
+export default ln
