d07f05c5a2302eecc123eab887855add0ef5c5a3
suqianru <510775597@qq.com>
2018-03-27 22:51:44 +0800

    新增重置密码相关多语言包

---
+++ src/common/lang/ko/password.js
@@ -0,0 +1,25 @@
+const ln = {
+  forgotPwd: '忘记密码',
+  // 提示
+  tips: {
+    account: '아이디를 입력해 주세요'
+  },
+  // 校验
+  validation: {
+  },
+
+  // 确认重置
+  auth: '确认重置',
+  send: '发送验证',
+  // 提示
+  authTips: {
+    email: '请输入邮箱验证码',
+    pwd: '输入新密码，仅限8-16位英文或者数字',
+    checkPwd: '确认新密码，与新密码相同',
+    safePwd: '输入新安全码，仅限8-16位英文或者数字',
+    checkSafePwd: '确认新安全码，与新安全码相同'
+  },
+  commit: '完成'
+}
+
+export default ln
